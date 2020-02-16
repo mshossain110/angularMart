2acce8f7b83df60c6cc52bd13398c8be9a3d69cb
mshossain110 <mshossain110@gmail.com>
2020-02-16 14:52:44 +0600

    Fix some issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn'
+    cdnify: 'grunt-cdnify'
   });
 
   // Configurable paths for the application
